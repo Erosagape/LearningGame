48dc1a39ef464e0cd7b7587390f848acaecd044c
Puttipong Kongpoung <leoputti@hotmail.com>
2021-09-22 09:59:57 +0700

    Update Auto Animation

--- Game1.cs
+++ Game1.cs
@@ -6,91 +6,91 @@ namespace LearningGame
 {
     public class Game1 : Game
     {
-        private GraphicsDeviceManager graphics;
-        private SpriteBatch spriteBatch;
-        private string currentKeyInput;
-        private string previousKeyInput;
-        private double secondsPass;
-        private string displayText = "";
-        private string displayTime = "";
-        private SpriteFont displayFont;
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        int spritePosX;
+        int spritePosY;
+        int spriteRow = 0;
+        int spriteIndex = 0;
+        int spriteSet = 0;
+        int spriteWidth = 50;
+        int spriteHeight = 48;
+        Texture2D chocoSource;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
-            currentKeyInput = "";
-            previousKeyInput = "";
-            displayText = "Previous = {0} Current = {1}";
-            secondsPass = 0;
+            Content.RootDirectory = "Content";          
         }
-
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            displayFont = Content.Load<SpriteFont>("Fonts/defaultfont");
+            chocoSource = Content.Load<Texture2D>("chocobo");
             // TODO: use this.Content to load your game content here
+            base.LoadContent();
         }
-
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed 
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed
                 || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+                
+            if(Keyboard.GetState().IsKeyUp(Keys.Space)){
+                if (spriteIndex < 2)
+                {
+                    spriteIndex++;
+                }                    
+                else
+                {
+                    spriteIndex = 0;
+                }
+            }
             
-            if (previousKeyInput != currentKeyInput && currentKeyInput != "")
+            if (Keyboard.GetState().IsKeyUp(Keys.Enter))
             {
-                previousKeyInput = currentKeyInput;
+                if (spriteRow < 3)
+                {
+                    spriteRow++;
+                }
+                else
+                {
+                    spriteRow = 0;
+                }
             }
-            currentKeyInput = "";
-            if (Keyboard.GetState().IsKeyDown(Keys.Left))
-            {
-                currentKeyInput = "Left";
-            }
-            else if (Keyboard.GetState().IsKeyDown(Keys.Right))
-            {
-                currentKeyInput = "Right";
-            }
-            else if (Keyboard.GetState().IsKeyDown(Keys.Up))
-            {
-                currentKeyInput = "Up";
-            }
-            else if (Keyboard.GetState().IsKeyDown(Keys.Down))
-            {
-                currentKeyInput = "Down";
-            }
-
-            secondsPass += gameTime.ElapsedGameTime.TotalSeconds;
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
+            graphics.GraphicsDevice.Clear(Color.White);
+            Viewport viewPort = graphics.GraphicsDevice.Viewport;
 
-            displayTime = "Game Time " + (int)secondsPass;
-            string showText = string.Format(displayText,previousKeyInput,currentKeyInput);
-            GraphicsDevice.Clear(Color.Blue);
-            Viewport vp = GraphicsDevice.Viewport;
+            spritePosX = (viewPort.Width - spriteWidth) / 2;
+            spritePosY = (viewPort.Height - spriteHeight) / 2; 
+            
+            Vector2 spritePosition = new Vector2(
+                spritePosX,spritePosY
+            );
+
+            int col_modifier = (spriteIndex * spriteWidth);
+            int row_modifier = (spriteRow * spriteHeight);
+
+            Rectangle characterPosition = new Rectangle(
+                spriteSet+row_modifier, spriteWidth+col_modifier,
+                spriteWidth,spriteHeight
+                );
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(chocoSource, spritePosition, characterPosition,Color.White);
+            spriteBatch.End();
             // TODO: Add your drawing code here
             base.Draw(gameTime);
-            spriteBatch.Begin();
-            spriteBatch.DrawString(displayFont, displayTime, Vector2.Zero, Color.Yellow);
-            spriteBatch.DrawString(
-                displayFont,
-                showText,
-                new Vector2(
-                    vp.Width / 2  - displayFont.MeasureString(showText).X / 2,
-                    vp.Height / 2 - displayFont.MeasureString(showText).Y / 2
-                    ),
-                Color.White);
-            spriteBatch.End();
         }
+
     }
 }
--- Lesson3.cs
+++ Lesson3.cs
@@ -13,6 +13,8 @@ namespace LearningGame
         int spritePosY = 48;
         int spriteMotion = 1;
         int spriteIndex = 0;
+        float frameCount;
+        bool isAnimated=true;
         KeyboardState previousKeyState;
         KeyboardState currentKeyState;
         public Lesson3()
@@ -35,52 +37,88 @@ namespace LearningGame
             spriteBatch = new SpriteBatch(graphics.GraphicsDevice);
             base.LoadContent();
         }
+        private void ChangeMotion()
+        {
+            if (spriteMotion == 2)
+            {
+                spriteMotion = 0;
+            }
+            else
+            {
+                spriteMotion += 1;
+            }
+        }
+        private void ChangeDirection()
+        {
+            if (spriteIndex == 3)
+            {
+                spriteIndex = 0;
+            }
+            else
+            {
+                spriteIndex += 1;
+            }
+        }
+
         protected override void Update(GameTime gameTime)
         {
+            frameCount += 1;
             previousKeyState = currentKeyState;
             currentKeyState = Keyboard.GetState();
+            bool isMove = false;
             if (currentKeyState.IsKeyUp(Keys.Enter) && previousKeyState.IsKeyDown(Keys.Enter))
             {
-                if (spriteMotion == 2)
-                {
-                    spriteMotion = 0;
-                }
-                else
-                {
-                    spriteMotion += 1;
-                }
+                ChangeMotion();
+                isMove = true;
             }
             if (currentKeyState.IsKeyUp(Keys.Space) && previousKeyState.IsKeyDown(Keys.Space))
             {
-                if (spriteIndex == 3)
+                ChangeDirection();
+                isMove = true;
+            }
+            if (currentKeyState.IsKeyUp(Keys.Escape) && previousKeyState.IsKeyDown(Keys.Escape))
+            {
+                isAnimated = !isAnimated;
+            }
+            if (isMove == false && isAnimated==true)
+            {
+                if((frameCount % 20) == 0)
                 {
-                    spriteIndex = 0;
-                }
-                else
-                {
-                    spriteIndex += 1;
+                    ChangeMotion();
+                    if (spriteMotion == 2)
+                    {
+                        ChangeDirection();
+                    }
                 }
             }
             base.Update(gameTime);
         }
         protected override void Draw(GameTime gameTime)
-        {
-            graphics.GraphicsDevice.Clear(Color.Black);
+        {            
+            graphics.GraphicsDevice.Clear(Color.White);
             
             Viewport viewPort = graphics.GraphicsDevice.Viewport;
+
             chocoYellow = new Rectangle(
                 spritePosX * spriteMotion, spritePosY * spriteIndex,
                 spritePosX, spritePosY
                 );
+
+            int frameWidth = spritePosX * 2;
+            int frameHeight = spritePosY * 2;
+
             Vector2 spritePos = new Vector2(
-                (viewPort.Width-spritePosX) /2,
-                (viewPort.Height-spritePosY) /2
+                (viewPort.Width-frameWidth) /2,
+                (viewPort.Height-frameHeight) /2
                 );
+
             Rectangle sourceRectangle = new Rectangle(
                 (int)spritePos.X, (int)spritePos.Y,
-                spritePosX,
-                spritePosY
+                frameWidth,
+                frameHeight
                 );
+
+            Window.Title = "FPS:" + frameCount + " MODE: " + (isAnimated ? "AUTO":"MANUAL");
 
             spriteBatch.Begin();
             spriteBatch.Draw(chocoSource, sourceRectangle,chocoYellow, Color.White);
